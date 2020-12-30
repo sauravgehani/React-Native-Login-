c5ee6de35dfb68a7cceff07ebf9b4bd3bcacc8f5
sauravgehani <76703640+sauravgehani@users.noreply.github.com>
2020-12-30 11:59:15 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Image, ScrollView } from 'react-native';
 
 
 
 export default function App() {
   return (
  <View style={styles.container}>
-
-    <Text>Welcome to my hybrid app, it works in both iOS and Android.</Text>
-    <TextInput
-      style={{ width:200, height: 40, borderColor: 'gray', borderWidth: 1 }}/>
-      
+   <ScrollView>
     
+    <Text> </Text>
+    <Text> </Text>
+    <Text>Register</Text>
+    <Text> </Text>
+
+    <Text>First Name</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text> </Text>
+
+    <Text>Last Name</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text> </Text>
+
+    <Text>Email Address</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text> </Text>
+
+    <Text>Password</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}
+      placeholder="Must be at least 8 digits long"/>
+    <Text> </Text>
+
+    <Text>Mobile Number</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text> </Text>
+
+    <Text>Address</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text> </Text>
+
+    <Text>City</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text></Text>
+
+    <Text>Zip Code</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text></Text>
+
+    <Text>State</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text></Text>
+
+    <Text>Country</Text>
+    <TextInput
+      style={{ width:200, height: 40, borderColor: 'black', borderWidth: 2 }}/>
+    <Text></Text>
+
+    <Button title = "Register"></Button>
+
+  </ScrollView>
 
     <StatusBar style="auto" />
+    
  </View>
   );
 
